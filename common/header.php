b26eb6f0bb97f56538422d5e57a943b8d9fae9d6
SantoAntlab <santhosh.b@antlab.io>
2022-10-17 12:36:45 +0530

    style fix

--- common/header.php
+++ common/header.php
@@ -1,70 +1,58 @@
-<nav class="navbar">
-      <div class="container-fluid bg-white shadow-header">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">
-            <img src="img/logo-main.png" alt="">
-          </a>
-        </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-          <ul class="nav navbar-nav">
-            <li class="dropdown">
-              <a class="dropdown-toggle" data-toggle="dropdown" href="#">CRM<span>
-                  <img src="img/down.svg" alt="">
-                </span>
-              </a>
-              <ul class="dropdown-menu">
-                <li>
-                  <a href="#">Stock Overview</a>
-                </li>
-                <li>
-                  <a href="#">Product Analysis</a>
-                </li>
-                <li>
-                  <a href="#">Products to be reordered</a>
-                </li>
-              </ul>
-            
-            <li>
-              <a href="#">My Sales Analysis</a>
-            </li>
-            <li>
-              <a href="#">Task</a>
-            </li>
+<nav class="navbar navbar-expand-sm bo-bottom remove-padding">
+  <div class="container-fluid bg-white">
+    <a class="navbar-brand" href="#"><img src="img/logo-main.png" alt=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav mr5">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">CRM <span><svg xmlns="http://www.w3.org/2000/svg" width="8.015" height="4.758" viewBox="0 0 8.015 4.758">
+            <path id="_9042707_nav_arrow_down_icon_1_" data-name="9042707_nav_arrow_down_icon (1)" d="M6,9l2.947,2.947L11.894,9" transform="translate(-4.939 -7.939)" fill="none" stroke="#7d7d7d" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"/>
+          </svg></a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Customer</a></li>
+            <li><a class="dropdown-item" href="#">Company</a></li>
+            <li><a class="dropdown-item" href="#">Quotes</a></li>
+            <li><a class="dropdown-item" href="#">Order</a></li>
+            <li><a class="dropdown-item" href="#">Invoice</a></li>
           </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <div class="nav-right-content fo-flx">
-              <div class="img-wr">
-                <img src="img/notification.svg" alt="">
-              </div>
-              <div class="content fo-flx">
-                <div class="prof fo-flx">
-                  <div class="dd">
-                    <li class="dropdown">
-                      <a class="dropdown-toggle" data-toggle="dropdown" href="#">Welcome</a>
-                      <ul class="dropdown-menu">
-                        <li>
-                          <a href="#">Page 1-1</a>
-                        </li>
-                        <li>
-                          <a href="#">Page 1-2</a>
-                        </li>
-                        <li>
-                          <a href="#">Page 1-3</a>
-                        </li>
-                      </ul>
-                    </li>
-                    <h6>Richard Welsh</h6>
-                  </div>
-                  <img src="img/prof-1.png" alt="">
-                </div>
-              </div>
+        </li>
+       
+        <li class="nav-item">
+          <a class="nav-link" href="#">My Sales Analysis</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Task</a>
+        </li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <div class="nav-right-content fo-flx">
+          <div class="img-wr">
+            <img src="img/notification.svg" alt="">
+          </div>
+          <div class="content fo-flx">
+            <div class="prof fo-flx">
+              <div class="dd">
+                <p>Welcome</p>
+                <h6>Richard Welsh</h6>
+              </div>       
+              
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"><img src="img/prof-1.png" alt=""></a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Customer</a></li>
+                  <li><a class="dropdown-item" href="#">Company</a></li>
+                  <li><a class="dropdown-item" href="#">Quotes</a></li>
+                  <li><a class="dropdown-item" href="#">Order</a></li>
+                  <li><a class="dropdown-item" href="#">Invoice</a></li>
+                </ul>
+              </li>
+              
             </div>
-          </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </ul>
+    </div>
+  </div>
+</nav>
